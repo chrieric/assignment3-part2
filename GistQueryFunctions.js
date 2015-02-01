ec28db875b918a442e52d9901f9ccd239a29e4eb
chrieric <chrieric@onid.oregonstate.edu>
2015-01-31 20:21:17 -0600

    Version 1.2
    Page now correctly functions by language but results are being
    duplicated. Need to determine source of duplication. Also, on initial
    search the page does not load, have to hit search again.

--- GistQueryFunctions.js
+++ GistQueryFunctions.js
@@ -5,8 +5,9 @@ function requestData()
 {
 	var request;
 	var init_url = "http://api.github.com/gists/public";
-	var pages =	3;
-	var page_size = 90;
+	var pages =	5;
+	var page_size = 30;
+	var temp_Array = [];
 
 	for(var i = 1;i <=pages;i++)
 	{
@@ -34,40 +35,41 @@ function requestData()
 			}
 		};	
 	}
+	
+	temp_Array = byLanguage(object_Array);
+	
+	createGistTable(document.getElementById('display-q'),temp_Array);
 };
+
 
 function arrayBuilder(ob_Array)
 {
-	var temp= [];
 	for(var i = 0;i < ob_Array.length;i++)
 	{
 		object_Array.push(ob_Array[i]);
-		//console.log(object_Array[i]);
 	}
-	
-	temp = byLanguage(object_Array);
-	
-	createGistTable(document.getElementById('display-q'),temp);
-}
-
-
+};
 
 function byLanguage(ob_Array)
 {
 	var temp = [];
 	
-	var pyth = document.getElementById('Python');
-	var jso = document.getElementById('JSON');
-	var js = document.getElementById('Javascript');
-	var sequel = document.getElementById('SQL');
+	var pyth_Select = document.getElementById('Python').checked;
+	var jso_Select = document.getElementById('JSON').checked;
+	var js_Select = document.getElementById('Javascript').checked;
+	var sequel_Select = document.getElementById('SQL').checked;
 	
-	var pyth_Select = pyth.value;
-	var jso_Select = jso.value;
-	var js_Select= js.value;
-	var sequel_Select = sequel.value;
+	//var pyth_Select = pyth.value;
+	//var jso_Select = jso.value;
+	//var js_Select= js.value;
+	//var sequel_Select = sequel.value;
 	
 	var lang_holder;
 	
+	console.log(pyth_Select+" is the status of pyth_Select");
+	console.log(jso_Select+" is the status of jso_Select");
+	console.log(js_Select+" is the status of js_Select");
+	console.log(sequel_Select+" is the status of sequel_Select");
 	for (var i = 0; i < ob_Array.length;i++)
 	{
 		var nested_Obj = ob_Array[i].files;
@@ -83,39 +85,37 @@ function byLanguage(ob_Array)
 					lang_holder = nested_Obj_2.language;
 				}			
 			}
-
 		}
 		
-		if(pyth_Select != 'Python' && jso_Select != 'JSON' && js_Select != 'Javascript' && sequel_Select != 'SQL')
+		if(pyth_Select == false && jso_Select == false && js_Select == false && sequel_Select == false)
 		{
-			
 			temp.push(ob_Array[i]);
-			
 		}
 		else
 		{
-			if(lang_holder == pyth_Select)
+			if(lang_holder == 'Python' && pyth_Select == true)
 			{
 				temp.push(ob_Array[i]);
 			}
 						
-			if(lang_holder == jso_Select)
+			if(lang_holder == 'JSON' && jso_Select == true)
+			{
+				temp.push(ob_Array[i]);
+				console.log(jso_Select);
+			}
+						
+			if(lang_holder == 'JavaScript' && js_Select == true)
 			{
 				temp.push(ob_Array[i]);
 			}
 						
-			if(lang_holder == js_Select)
-			{
-				temp.push(ob_Array[i]);
-			}
-						
-			if(lang_holder == sequel_Select)
+			if((lang_holder == 'SQL' || lang_holder == 'sql') && sequel_Select == true)
 			{
 				temp.push(ob_Array[i]);
 			}
 		}
 	}
-	console.log(temp[i]);
+	
 	return temp;
 	
 };
@@ -124,6 +124,7 @@ function byLanguage(ob_Array)
 function createGistTable(ul,qArray)
 {	
 	_resetTable(ul);
+	
 	var page_num = document.getElementById('per-page');
 	var page_num_value = page_num.value;
 	var page_size = 30;
